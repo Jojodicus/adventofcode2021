dbcb216e118585f6ee849e278a199b208138558b
Johannes <76924386+Jojodicus@users.noreply.github.com>
2021-12-03 12:33:26 +0100

    updated to use transpose

--- day3/BinaryDiagnostic.scala
+++ day3/BinaryDiagnostic.scala
@@ -1,22 +1,17 @@
 import scala.io.Source
 
 object BinaryDiagnostic {
-  def mostCommon(in: List[String]): IndexedSeq[Int] =
-    (for (i <- 0 until in(0).length) // iterate row
-      yield (for (str <- in) // iterate col
-        yield str.charAt(i)
-        ).count(_ == '1') // count '1's
-      ).map(a => if (a << 1 >= in.length) 1 else 0) // map digit according to frequency
-
+  def mostCommon(in: List[String]): List[Int] =
+    (for (e <- in.transpose) yield e.count(_ == '1')).map(a => if (a << 1 >= in.length) 1 else 0)
 
   def puzzleOne(in: List[String]): Int = {
-    val width = in(0).length
+    val mc = mostCommon(in)
 
-    // convert encoded binary list to int
-    val gamma = (for t <- mostCommon(in).zipWithIndex yield t._1 << (width - t._2 - 1)).sum
+    // convert encoded binary to decimal
+    val gamma = (for t <- mc.zipWithIndex yield t._1 << (mc.length - t._2 - 1)).sum
 
     // multiply with bitwise negation
-    gamma * (~gamma & (-1 >>> (32 - width)))
+    gamma * (~gamma & (-1 >>> (32 - mc.length)))
   }
 
   def puzzleTwo(in: List[String]): Int = {
@@ -27,7 +22,7 @@ object BinaryDiagnostic {
     var i = 0
     while (results.length != 1) do { // continuously filter
       results = results.filter(s => s.charAt(i) == mostCommon(results)(i) + '0')
-      i = i + 1
+      i += 1
     }
     // decode remaining element
     val resO2 = (for t <- results(0).zipWithIndex yield t._1 - '0' << (width - t._2 - 1)).sum
@@ -37,7 +32,7 @@ object BinaryDiagnostic {
     i = 0
     while (results.length != 1) do {
       results = results.filter(s => s.charAt(i) != mostCommon(results)(i) + '0')
-      i = i + 1
+      i += 1
     }
     val resCO2 = (for t <- results(0).zipWithIndex yield t._1 - '0' << (width - t._2 - 1)).sum
 
