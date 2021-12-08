25bec12dc67cfa1691fa7658d2860878e14cd81c
Johannes <76924386+Jojodicus@users.noreply.github.com>
2021-12-08 12:41:37 +0100

    further updates
    only numbers 2,3 and 5 remain

--- day8/sevenSegmentSearch.js
+++ day8/sevenSegmentSearch.js
@@ -33,14 +33,129 @@ function puzzleOne(data) {
 function puzzleTwo(data) {
     let result = 0;
 
-    for (let i = 0; i < data.length; i++) {
-        result += decode(data[i]);
-    }
+    decode(data[0]);
+    // for (let i = 0; i < data.length; i++) {
+    //     result += decode(data[i]);
+    // }
 
     return result;
 }
 
 function decode(data) {
+    let input = data[0];
+
+    // decode input
+    let coded = new Array(10).fill(""); // digits as they are encoded in the input
+    let segments = new Array(7).fill(""); // segments as they are encoded in the input
+
+    // numbers 1, 4, 7, 8
+    for (let i = 0; i < input.length; i++) {
+        switch (input[i].length) {
+            case 2:
+                coded[1] = input[i];
+                break;
+            case 3:
+                coded[7] = input[i];
+                break;
+            case 4:
+                coded[4] = input[i];
+                break;
+            case 7:
+                coded[8] = input[i];
+                break;
+        }
+    }
+
+    console.log(coded);
+
+    // segment a
+    // 1 and 7 -> a
+    for (let i = 0; i < 3; i++) {
+        if (!(coded[1].includes(coded[7].charAt(i)))) {
+            segments[0] = coded[7].charAt(i);
+            break;
+        }
+    }
+
+    // segment g, number 9
+    // 4 and 9 and a -> g
+    for (let i = 0; i < input.length; i++) {
+        var curr = input[i];
+
+        if (curr.length != 6) {
+            continue;
+        } // only 0, 6, 9 remain
+
+        // check if all segments of 4 and a are included
+        var remaining = "";
+        for (let j = 0; j < curr.length; j++) {
+            var cchar = curr.charAt(j);
+            if (cchar == segments[0] || coded[4].includes(cchar)) {
+                continue;
+            }
+            // neither in a or 4
+            remaining += cchar;
+        }
+
+        // only one extra char -> g in 9
+        if (remaining.length == 1) {
+            coded[9] = curr;
+            segments[6] = remaining;
+            break;
+        }
+    }
+
+    // segment e
+    // in 8 but not in 9 -> e
+    for (let i = 0; i < coded[8].length; i++) {
+        if (!coded[9].includes(coded[8].charAt(i))) {
+            segments[4] = coded[8].charAt(i);
+            break;
+        }
+    }
+
+    // segment b, number 0
+    // not in 1, nor a/e/g -> b
+    for (let i = 0; i < input.length; i++) {
+        var curr = input[i];
+        
+        if (curr.length != 6 || curr == coded[9]) {
+            continue;
+        } // only 0, 6 remain
+
+        // check if all segments of 1, a, e, g are included
+        var remaining = "";
+        for (let j = 0; j < curr.length; j++) {
+            var cchar = curr.charAt(j);
+            if (cchar == segments[0] || cchar == segments[4] || cchar == segments[6] || coded[1].includes(cchar)) {
+                continue;
+            }
+            // neither in a or 4
+            remaining += cchar;
+        }
+
+        // only one extra char -> b in 0
+        if (remaining.length == 1) {
+            coded[0] = curr;
+            segments[2] = remaining;
+            break;
+        }
+    }
+
+    // number 6
+    // length 6 but neiter 0 nor 9 -> 6
+    for (let i = 0; i < input.length; i++) {
+        var curr = input[i];
+
+        if (curr.length != 6 || curr == coded[0] || curr == coded[9]) {
+            continue;
+        } // only 6 remains
+
+        coded[6] = curr;
+    }
+
+    console.log(segments);
+
     // todo
     return 42;
 }
