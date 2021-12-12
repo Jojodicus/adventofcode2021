17a9c825788d1f56bc6ab8e52de3b5b48befb7ec
Johannes <76924386+Jojodicus@users.noreply.github.com>
2021-12-12 18:47:13 +0100

    adjacency set instead of matrix

--- day12/passagePathing.py
+++ day12/passagePathing.py
@@ -1,4 +1,4 @@
-edges: list[list[int]] = None
+edges: list[set[int]] = None
 visited: set = set()
 visitedTwice = None
 
@@ -18,9 +18,9 @@ def p1helper(current: int) -> int:
         return 1
 
     total = 0
-    for i, v in enumerate(edges[current]):
+    for i in edges[current]:
         # extensions
-        if v == 0 or i2n[i] == "start" or i in visited:
+        if i2n[i] == "start" or i in visited:
             continue
 
         # apply
@@ -52,9 +52,9 @@ def p2helper(current: int) -> int:
         visited.add(current)
 
     total = 0
-    for i, v in enumerate(edges[current]):
+    for i in edges[current]:
         # extensions
-        if v == 0 or i2n[i] == "start":
+        if i2n[i] == "start":
             continue
 
         # backtrack
@@ -72,7 +72,7 @@ def parseInput(input: str) -> None:
     lines = input.split('\n')
     connections = [line.split('-') for line in lines]
     
-    # create a bidirectional dictionary of all the names
+    # create bidirectional dictionary of all names
     counter = 0
     for connection in connections:
         for name in connection:
@@ -81,12 +81,12 @@ def parseInput(input: str) -> None:
                 i2n[counter] = name
                 counter += 1
 
-    # create a numpy array of all the edges
+    # create set of edges
     global edges
-    edges = [[0 for _ in range(counter)] for _ in range(counter)]
+    edges = [set() for _ in range(counter)]
     for connection in connections:
-        edges[n2i[connection[0]]][n2i[connection[1]]] = 1
-        edges[n2i[connection[1]]][n2i[connection[0]]] = 1
+        edges[n2i[connection[0]]].add(n2i[connection[1]])
+        edges[n2i[connection[1]]].add(n2i[connection[0]])
 
 if __name__ == "__main__":
     with open("input", 'r') as f:
