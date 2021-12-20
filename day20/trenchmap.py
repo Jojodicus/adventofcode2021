1dae941f6e5e1315a245e332f499ea8110d05520
Johannes <76924386+Jojodicus@users.noreply.github.com>
2021-12-20 14:24:22 +0100

    day 20: trench map

--- day20/trenchmap.py
+++ day20/trenchmap.py
@@ -5,11 +5,58 @@ enhancer: list[bool] = []
 
 
 def puzzleOne() -> int:
-    pass
+    # enhance twice
+    enhance()
+    enhance()
+    return lit()
 
 
 def puzzleTwo() -> int:
-    pass
+    # already enhanced twice
+    for _ in range(48):
+        enhance()
+    return lit()
+
+
+def enhance():
+    global image
+    global outsideLit
+
+    # boundry to not interfere with outsidelit
+    minX = min(x for x, _ in image)
+    maxX = max(x for x, _ in image)
+    minY = min(y for _, y in image)
+    maxY = max(y for _, y in image)
+
+    newImage: set[tuple[int, int]] = set()
+
+    for x in range(minX - 1, maxX + 2):
+        for y in range(minY - 1, maxY + 2):
+            index = 0
+
+            # neighbours
+            for dy in (-1, 0, 1):
+                for dx in (-1, 0, 1):
+                    index <<= 1
+
+                    # lit in image
+                    if (x + dx, y + dy) in image:
+                        index |= 1
+                    # lit in outside
+                    elif outsideLit:
+                        if x + dx < minX or x + dx > maxX or y + dy < minY or y + dy > maxY:
+                            index |= 1
+
+            # enhance
+            if enhancer[index]:
+                newImage.add((x, y))
+
+    # update image
+    image = newImage
+    if outsideLit:
+        outsideLit = enhancer[-1]
+    else:
+        outsideLit = enhancer[0]
 
 
 def lit() -> int:
@@ -23,13 +70,12 @@ def parseInput(input: str):
     algorithm, imageData = input.split("\n\n")
 
     # enhancement algorithm
-    algorithm = algorithm.replace("\n", "")
     enhancer = [True if c == "#" else False for c in algorithm]
 
     # image data
     imageData = [list(line) for line in imageData.split("\n")]
     for y, line in enumerate(imageData):
-        for x, char in enumerate(y):
+        for x, char in enumerate(line):
             if char == "#":
                 image.add((x, y))
 
@@ -38,3 +84,5 @@ if __name__ == '__main__':
     with open("input", 'r') as f:
         parseInput(f.read())
     
+    print("Part One:", puzzleOne())
+    print("Part Two:", puzzleTwo())
